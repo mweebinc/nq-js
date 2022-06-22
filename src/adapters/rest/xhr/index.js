02fe1a2a580c7b60440cb5c6da6569756791b7b5
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-06-22 17:05:11 +0800

    todo fix error : TypeError: this.localStorage.getItem is not a function

--- src/adapters/rest/xhr/index.js
+++ src/adapters/rest/xhr/index.js
@@ -1,5 +1,10 @@
 // import XhrAdapter from './XhrAdapter';
 const  XhrAdapter = require('./XhrAdapter')
+const GetLocalStorageAdapter = require("../../cache/local/index");
+const UserCache = require('../../../data/user/source/UserCache');
+
+const memoryAdapter = new GetLocalStorageAdapter(NaN)
+const userCache = new UserCache(memoryAdapter)
 
 function getXhrAdapter() {
     let XHR = null;
@@ -8,7 +13,7 @@ function getXhrAdapter() {
     } else {
         throw new Error('XMLHttpRequest was found.');
     }
-    return new XhrAdapter(XHR);
+    return new XhrAdapter(XHR, userCache);
 }
 
 // export default getXhrAdapter;
