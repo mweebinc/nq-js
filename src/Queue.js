4a3f2be8a855a04f375b888c7dc8debb68f1d55b
John Carlo Franco <francojohnc@gmail.com>
2022-09-23 11:00:14 +0800

    add email

--- src/Queue.js
+++ src/Queue.js
@@ -2,6 +2,7 @@ const Collection = require('./domain/Collection');
 const Document = require('./domain/Document');
 const User = require('./domain/User');
 const File = require('./domain/File');
+const Email = require('./domain/Email');
 const Config = require('./Config');
 const LiveQuery = require('./livequery/LiveQuery');
 
@@ -9,6 +10,7 @@ class Queue {
     static setUrl(value) {
         Config.set('SERVER_URL', value);
     }
+
     static setApplicationId(value) {
         Config.set('APPLICATION_ID', value);
     }
@@ -19,4 +21,5 @@ Queue.Collection = Collection;
 Queue.Document = Document;
 Queue.LiveQuery = LiveQuery;
 Queue.File = File;
+Queue.Email = Email;
 module.exports = Queue;
